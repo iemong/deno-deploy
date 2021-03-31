e9b35f97d21ae3338af94a119d078947a6585696
iemong <kiyoshidainagon@gmail.com>
2021-03-31 23:16:07 +0900

    ルートにアクセスした時はとりあえず200返す。fetchのbodyを渡す

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
-async function handleRequest(request) {
+async function handleRequest(request:any) {
     const { pathname } = new URL(request.url);
 
     if (pathname.startsWith("/weather")) {
         const res = await fetch('https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json')
-        const json = await res.json()
-
-        return new Response(json, {
+        return new Response(res.body, {
             headers: {
                 "content-type": "application/json; charset=UTF-8",
             },
         });
     }
+
+
+    return new Response(
+        "<html> Hello </html>",
+        {
+            status: 200,
+            headers: {
+                "content-type": "text/html; charset=UTF-8",
+            },
+        },
+    );
 }
 
 addEventListener("fetch", (event) => {
